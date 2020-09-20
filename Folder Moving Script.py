b55a8f3fbc1de2cad54f088f571a2b3d9da8aa42
Donovan Grant <35610158+Obama-chan@users.noreply.github.com>
2020-09-20 17:37:48 -0400

    Add files via upload
    Adding script to repository at the moment the program works bu taking the input of where the user is pulling the folder or file from and then asking which directory they want to send it to.

---
+++ Folder Moving Script.py
@@ -0,0 +1,28 @@
+#Sorting Files
+import os
+import sys
+import shutil
+
+#Get path info from user
+Print ("Option 1: Moving a Directory to another directory.\n" + "Option 2: Moving a file to a directory\n"
+
+
+#r'C:\Users\Obama-Chan\Desktop\Source'
+path = input("Gimme the path to folder you want to move Files from:\n")
+print ("Starting path is: " + path)
+
+#destination = r'C:\Users\Obama-Chan\Desktop\Destination'
+dest = input("Now give me the path of the folder you want to move the files to:\n")
+print ("Destination path is:" + dest) 
+
+
+
+
+folders = os.listdir(path)
+#porn is each directory in Sorted Doujins
+for folder in folders:
+        folderName = input("Enter Full name of file or keyword in filename:\n")
+        if folderName.lower() in folder.lower():
+                shutil.move(os.path.join(path, folder), dest)
+        print (folder + ' succesfully moved')
+
