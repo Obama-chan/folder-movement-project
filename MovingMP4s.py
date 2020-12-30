d2c436521dd00ed84c2e0b580db740d36027ab46
Donovan Grant <35610158+Obama-chan@users.noreply.github.com>
2020-12-29 22:58:07 -0500

    Create MovingMP4s.py
    Code similar to first script, I have an if else for duplicates for need to look into try and except

---
+++ MovingMP4s.py
@@ -0,0 +1,30 @@
+#Moving MP4s
+import os
+import sys
+import shutil
+
+#source and destination are arguments
+source_dir = sys.argv[1]
+dest_dir = sys.argv[2]
+
+print(source_dir)
+print(dest_dir)
+
+creator_folders = os.listdir(dest_dir)
+for creator in creator_folders:
+    for video in os.listdir(source_dir):
+        if creator.lower() in video.lower():
+            print('check')
+            src = os.path.join(source_dir, video)
+            print(src + '\n')
+            dest = os.path.join(dest_dir, creator)
+            print(dest + '\n')
+            #if statement to check if duplicate exists
+            if os.path.exists(source_dir + '/' + video) and os.path.exists(dest_dir + '/' + creator + '/' + video):
+                #os.remove(source_dir + '/' + video) this is for video moving
+                shutil.rmtree(source_dir + '/' + video)
+                print(video + 'deleted due to being a duplicate\n')
+            else:
+                shutil.move(src, dest)
+                print(video + ' has been successfully moved\n')
+                
