e3ee59510b407deaaf2de85e1843d14ead7d41ad
Donovan Grant <35610158+Obama-chan@users.noreply.github.com>
2021-01-17 12:06:49 -0500

    Update Folder Moving Script.py
    If else statement to remove duplicate folders

--- Folder Moving Script.py
+++ Folder Moving Script.py
@@ -4,18 +4,26 @@ import os
 import sys
 import shutil
 
-src_dir = sys.argv[1]
+#source and destination are arguments
+source_dir = sys.argv[1]
 dest_dir = sys.argv[2]
 
-print(src_dir)
+print(source_dir)
 print(dest_dir)
 
-artists = os.listdir(dest_dir)
-
-for artist in artists:
-	for folder in os.listdir(src_dir):
-		if artist.lower() in folder.lower():
-			src = os.path.join(src_dir, folder)
-			dest = os.path.join(dest_dir, artist)
-			shutil.move(src, dest)
-			print (folder + ' succesfully moved')
+creator_folders = os.listdir(dest_dir)
+for creator in creator_folders:
+    for video in os.listdir(source_dir):
+        if creator.lower() in video.lower():
+            print('check')
+            src = os.path.join(source_dir, video)
+            print(src + '\n')
+            dest = os.path.join(dest_dir, creator)
+            print(dest + '\n')
+            #if statement to check if duplicate exists
+            if os.path.exists(source_dir + '/' + video) and os.path.exists(dest_dir + '/' + creator + '/' + video):
+                shutil.rmtree(source_dir + '/' + video) #Removes duplicate folder
+                print(video + ' deleted due to being a duplicate\n')
+            else:
+                shutil.move(src, dest)
+                print(video + ' has been successfully moved\n')
